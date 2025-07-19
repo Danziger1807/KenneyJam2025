c9291b4c14cc375d10b99d3b71f4b0cb5f05972c
Danziger <Pafik2005@wp.pl>
2025-07-19 09:54:21 +0200

    fixed map transition

--- Assets/MapTransition.cs
+++ Assets/MapTransition.cs
@@ -1,11 +1,15 @@
 using Unity.Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MapTransition : MonoBehaviour
 {
     [SerializeField] PolygonCollider2D mapBoundry;
     CinemachineConfiner2D confider;
+    [SerializeField] Direction direction;
+    [SerializeField] float additivePos = 1.75f;
 
+    enum Direction {  Up, Down, Left, Right}
     private void Awake()
     {
         confider = FindAnyObjectByType<CinemachineConfiner2D>();
@@ -17,8 +21,35 @@ public class MapTransition : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             confider.BoundingShape2D = mapBoundry;
+            UpdatePlayerPosition(collision.gameObject);
         }
     }
 
+    private void UpdatePlayerPosition(GameObject Player)
+    {
+        Vector3 newPos = Player.transform.position;
+
+        switch (direction)
+        {
+            case Direction.Up:
+                newPos.y += additivePos;
+                break;
+
+            case Direction.Down:
+                newPos.y -= additivePos;
+                break;
+            case Direction.Left:
+                newPos.x -= additivePos;
+                break;
+
+            case Direction.Right:
+                newPos.x += additivePos;
+                break;
+
+        }
+        Player.transform.position = newPos;
+    }
+
+    
 
 }
